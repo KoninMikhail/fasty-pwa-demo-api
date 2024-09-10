3da1eef64447c3fc5b4b1ed3fbcd9a9ecfe2e572
MikhailKonin <dev.konin@gmail.com>
2024-09-11 02:46:25 +0300

    fix url encoded

--- src/app.ts
+++ src/app.ts
@@ -52,10 +52,10 @@ app.use(
 
 
 // parse json request body
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // parse urlencoded request body
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // sanitize request data
 app.use(xss());
